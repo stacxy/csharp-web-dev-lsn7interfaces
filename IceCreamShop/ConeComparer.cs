5614a3c8b80c5b75eaa80947cc238dc7ed3442f9
stacey <staceystroh.84@gmail.com>
2020-12-30 00:38:23 -0600

    .

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    { }
+
+    public ConeComparer()
+    {
+    }
+
+    public int Compare(Cone x, Cone y)
+    {
+        double diff = x.Cost - y.Cost;
+        if(diff == 0)
+        {
+            return 0;
+        }
+        else if(diff < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+        }
+
+    
+    }
+}
